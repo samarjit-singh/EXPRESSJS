52f64e016fe28484c7faba8ed442da7edb035afe
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-06-16 13:04:52 +0530

    basics of express

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
+// middle ware
+// const samarMiddleware = (res, req, next) => {
+//   console.log(req);
+//   next();
+// };
+
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(samarMiddleware);
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Heloo world " + req.params.name);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  // res.status(500);
+  res.json({ samar: 20 });
 });
 
 app.listen(port, () => {
